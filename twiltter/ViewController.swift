570d9e30f44e5f389f849ec7b36482d09fe7a54d
ltran6 <Leland_Tran@Intuit.com>
2017-04-10 21:15:45 -0700

    Initial Commit

---
+++ twiltter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twiltter
+//
+//  Created by Tran, Leland on 4/10/17.
+//  Copyright © 2017 Tran, Leland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ twiltterTests/twiltterTests.swift
@@ -0,0 +1,36 @@
+//
+//  twiltterTests.swift
+//  twiltterTests
+//
+//  Created by Tran, Leland on 4/10/17.
+//  Copyright © 2017 Tran, Leland. All rights reserved.
+//
+
+import XCTest
+@testable import twiltter
+
+class twiltterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
